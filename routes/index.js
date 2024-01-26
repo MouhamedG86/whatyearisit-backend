19246e5f59b84885d541419b57610b11d03f3650
mouhamed gueye <gueyenioro2012@gmail.com>
2024-01-26 16:39:47 +0100

    -test

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-let  year = new Date().getUTCFullYear().toString();
+let  year = new Date().getUTCFullYear();
 
 
 /* GET home page. */
 router.get('/year', function(req, res, next) {
-  res.json({ year: year });
+  res.json({ year: year.toString() });
 });
 
 module.exports = router;
